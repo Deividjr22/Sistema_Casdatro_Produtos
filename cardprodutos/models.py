bbce1c38b951e3dcfbce8b64d8d46ecd5d24b527
Deivid <deividjrscasanova@gmail.com>
2026-08-03 21:47:30 -0300

    08.03.2026
    falta corrigir o admin

--- cardprodutos/models.py
+++ cardprodutos/models.py
@@ -1,4 +1,72 @@
 from django.db import models
 
+# Create your models here.
+from django.db import models
 
+class Brand(models.Model):
+    name= models.CharField(max_length=100,verbose_name='Nome')
+    is_active = models.BooleanField(default=True,verbose_name='Ativo')
+    descripiton = models.TextField(null=True,verbose_name='Descrição')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='criado em')
+    update_at = models.DateTimeField(auto_now=True,verbose_name='Atualizado em')
+
+    class meta:
+        ordering =['name']
+        verbose_name = 'Marca'
+
+    def __str__(self):
+        return self.name
     
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name ='Nome')
+    is_active = models.BooleanField(default=True, verbose_name ='Ativo')
+    description = models.TextField(null=True,blank=True, verbose_name ='Descrição')
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name ='Criado em')
+    updated_at = models.DateTimeField(auto_now = True, verbose_name ='Atualizado em')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Categoria'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Titulo')
+    brand = models.ForeignKey(Brand,on_delete=models.PROTECT,related_name='products',verbose_name='Marca') 
+    from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Brand(models.Model):
+    name= models.CharField(max_length=100,verbose_name='Nome')
+    is_active = models.BooleanField(default=True,verbose_name='Ativo')
+    descripiton = models.TextField(null=True,verbose_name='Descrição')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='criado em')
+    update_at = models.DateTimeField(auto_now=True,verbose_name='Atualizado em')
+
+    class meta:
+        ordering =['name']
+        verbose_name = 'Marca'
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name ='Nome')
+    is_active = models.BooleanField(default=True, verbose_name ='Ativo')
+    description = models.TextField(null=True,blank=True, verbose_name ='Descrição')
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name ='Criado em')
+    updated_at = models.DateTimeField(auto_now = True, verbose_name ='Atualizado em')
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Categoria'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Titulo')
+    brand = models.ForeignKey(Brand,on_delete=models.PROTECT,related_name='products',verbose_name='Marca') 
